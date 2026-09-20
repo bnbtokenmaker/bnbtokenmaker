535015877fb6a230776321b345a88f35dbcd775d
BNB Token Maker <support@bnbtokenmaker.com>
2026-09-20 21:32:19 +0300

    Prepare standalone cPanel deployment pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,6 @@ const eslintConfig = defineConfig([
       "@next/next/no-img-element": "off",
     },
   },
-  {
-    files: ["server.js"],
-    rules: {
-      "@typescript-eslint/no-require-imports": "off",
-    },
-  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
